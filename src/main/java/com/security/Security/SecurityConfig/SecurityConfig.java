d242ed7c093886d73eeb8b96b774f05832a62a84
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-21 19:04:47 +0100

    made a use of in-memory authentication to create multiple users, and manging this in memory

--- src/main/java/com/security/Security/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/security/Security/SecurityConfig/SecurityConfig.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +20,15 @@ public class SecurityConfig {
         http.authorizeHttpRequests((requests) -> {
             ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated();
         });
-        http.formLogin(Customizer.withDefaults());
+        //http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain) http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}admin1").roles("ADMIN").build();
+        return new InMemoryUserDetailsManager(user1,admin);
     }
 }
