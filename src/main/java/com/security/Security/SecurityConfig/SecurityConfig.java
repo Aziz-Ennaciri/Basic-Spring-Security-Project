347913eba7164c35349fad06e683b80a8874cfc2
Aziz-Ennaciri <azizennaciri0@gmail.com>
2024-08-24 03:32:01 +0100

    updating SecurityConfig and add @bean AuthenticationManager

--- src/main/java/com/security/Security/SecurityConfig/SecurityConfig.java
+++ src/main/java/com/security/Security/SecurityConfig/SecurityConfig.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -86,5 +88,9 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
             return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
